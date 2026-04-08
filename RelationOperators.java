1e26dfa75d24786a1e0938bf671b75cd8d21e8a5
mcabhishek <mcabhishek2002@gmail.com>
2026-04-08 14:39:35 +0530

    all form

---
+++ RelationOperators.java
@@ -0,0 +1,18 @@
+public class RelationOperators
+{
+    public static void main(String args[])
+    {
+        int a = 10;
+        int b = 20;
+        int c = a - b;
+        int d = b / 10;
+
+        System.out.println("is a!=b " + (a != b));
+        System.out.println("is a<b " + (a < b));
+        System.out.println("is a==b " + (a == b));
+        System.out.println("is a<=b " + (a <= b));
+        System.out.println("is a>b " + (a > b));
+        System.out.println("is a<=c " + (a <= c));
+        System.out.println("is a>=d " + (a >= d));
+    }
+}
