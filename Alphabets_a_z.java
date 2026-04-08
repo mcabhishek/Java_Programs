ce1849ea21f76b405773bbe34608b82aeed9e4f1
mcabhishek <mcabhishek2002@gmail.com>
2026-04-08 14:35:33 +0530

    all form

---
+++ Alphabets_a_z.java
@@ -0,0 +1,12 @@
+class Alphabets_a_z
+{
+    public static void main(String args[])
+    {
+        char c='a';
+        for (int i=0;i<26;i++)
+        {
+            System.out.print(c+" ");
+            c++;
+        }
+    }
+}
