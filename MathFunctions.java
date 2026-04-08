862e70c9c77e44dbade979020a3719ec24fbee73
mcabhishek <mcabhishek2002@gmail.com>
2026-04-08 14:16:19 +0530

    alll

---
+++ MathFunctions.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class MathFunctions
+{
+    public static void main(String args[])
+    {
+        System.out.println(Math.PI);
+
+        System.out.println("square root of 9 is : " + Math.sqrt(9));
+
+        System.out.println("2 to the power 6 is : " + Math.pow(2, 6));
+
+        System.out.println("The absolute value of -5 : " + Math.abs(-5));
+
+        System.out.println("the round value of 3.3453453453 : " + Math.round(3.3453453453));
+
+        System.out.println("the ceiling value of 3.3453453453 : " + Math.ceil(3.3453453453));
+
+        System.out.println("the floor value of 3.3453453453 : " + Math.floor(3.3453453453));
+
+        System.out.println("the max value between 4,5 : " + Math.max(4, 5));
+
+        System.out.println("the min value between 4,5 : " + Math.min(4, 5));
+    }
+}
